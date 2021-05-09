a456d742996d730b52dbbbdf0d23f4d547da8da8
Frankie Bagnardi <f.bagnardi@gmail.com>
2021-05-08 20:53:35 -0700

    test(macro): add vec test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-// macro_rules! eval {
-//     ($($tt:tt)*) => {{
-//         let js = stringify!($($tt)*);
-//         todo!("implement compile time eval")
-//     }};
-// }
-
 #[cfg(test)]
 mod tests {
     use ctjs_macros::eval;
@@ -16,5 +9,16 @@ mod tests {
             x * 3.5
         };
         assert_eq!(y, 10.5);
+    }
+
+    #[test]
+    fn it_handles_more_complexity() {
+        let nums: Vec<f64> = eval! {
+            const values = Array.from({ length: 30 }, (x, i) => Math.sin(i / (Math.PI * 2)));
+            "vec![" + values.map(value => value % 1 ? value : value + ".0") + "]"
+        };
+        println!("Nums: {:#?}", nums);
+
+        assert_eq!("making test fail to see stdout", "");
     }
 }
